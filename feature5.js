e2234c076d66e8d20f152a4fc72f49411c115a8f
ArnaudoAnnA <38939245+ArnaudoAnnA@users.noreply.github.com>
2018-10-08 09:57:23 +0200

    Update feature5.js

--- feature5.js
+++ feature5.js
@@ -25,19 +25,19 @@
 
 	function invia_dati(servURL, params, method) 
 	{
-    method = method || "post"; // il metodo POST è usato di default
-    var form = document.createElement("form");
-    form.setAttribute("method", method);
-    form.setAttribute("action", servURL);
-    for(var key in params) {
-        var hiddenField = document.createElement("input");
-        hiddenField.setAttribute("type", "hidden");
-        hiddenField.setAttribute("name", key);
-        hiddenField.setAttribute("value", params[key]);
-        form.appendChild(hiddenField);
-    }
-    document.body.appendChild(form);
-    form.submit();
+	    method = method || "post"; // il metodo POST è usato di default
+	    var form = document.createElement("form");
+	    form.setAttribute("method", method);
+	    form.setAttribute("action", servURL);
+	    for(var key in params) {
+		var hiddenField = document.createElement("input");
+		hiddenField.setAttribute("type", "hidden");
+		hiddenField.setAttribute("name", key);
+		hiddenField.setAttribute("value", params[key]);
+		form.appendChild(hiddenField);
+	    }
+	    document.body.appendChild(form);
+	    form.submit();
 	}
 	
 	
@@ -45,34 +45,34 @@
                                                             ATTENZIONE: l'attributo name degli oggetti html che rappresentano le carte deve corrispondere al numero della carta 
                                                         **/
 	{
-        /*copio i valori delle variabili ricevute tramite PHP in un vettore da inviare dinuovo al server per richiedere la prossima pagina*/
-		var params = new Array();
-		params['n_giocatori'] = <?php echo $n_giocatori; ?>;
-		
-        params['nomi_giocatori'] = new array();
-        params['nomi_giocatori'].forEach(phpArrayToJS(item, index){
-			item = <?php echo $nomi_giocatori[index]?>;
-		});
-		
-		params['punteggio_giocatori'] = new array();
-        params['punteggio_giocatori'].forEach(phpArrayToJS(item, index){
-			item = <?php echo $punteggio_giocatori[index]?>;
-		});
-		
-        params['nome_giocatore_corrente'] = <?php echo $nome_giocatore_corrente; ?>;
-        
-        
-        /* ricavo il numero della carta selezionata*/
-        params['carta_cliccata'] = cartaCliccata.name;
-        
-        params['carte_in_mano'] = new array();
-		params['carte_in_mano'].forEach(phpArrayToJS(item, index){
-				item = <?php echo $carte_in_mano[index]?>;
-		});
-        
-        var servURL = <?php echo servURL?>;
-        
-        invia_dati(servURL, params, post); 
+		/*copio i valori delle variabili ricevute tramite PHP in un vettore da inviare dinuovo al server per richiedere la prossima pagina*/
+			var params = new Array();
+			params['n_giocatori'] = <?php echo $n_giocatori; ?>;
+
+		params['nomi_giocatori'] = new array();
+		params['nomi_giocatori'].forEach(phpArrayToJS(item, index){
+				item = <?php echo $nomi_giocatori[index]?>;
+			});
+
+			params['punteggio_giocatori'] = new array();
+		params['punteggio_giocatori'].forEach(phpArrayToJS(item, index){
+				item = <?php echo $punteggio_giocatori[index]?>;
+			});
+
+		params['nome_giocatore_corrente'] = <?php echo $nome_giocatore_corrente; ?>;
+
+
+		/* ricavo il numero della carta selezionata*/
+		params['carta_cliccata'] = cartaCliccata.name;
+
+		params['carte_in_mano'] = new array();
+			params['carte_in_mano'].forEach(phpArrayToJS(item, index){
+					item = <?php echo $carte_in_mano[index]?>;
+			});
+
+		var servURL = <?php echo servURL?>;
+
+		invia_dati(servURL, params, post); 
 	}
 	
 //</script>	

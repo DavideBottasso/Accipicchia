adf6b7c360ff7f993e4a658570e39e56d43c32b9
ArnaudoAnnA <38939245+ArnaudoAnnA@users.noreply.github.com>
2018-11-05 11:19:30 +0100

    Update feature6.php

--- feature6.php
+++ feature6.php
@@ -1,12 +1,9 @@
 <?php
 	#parametri passati alla pagina : 	
-	#variabili definite in PHP:
-		#n_giocatori
-		#giocatori (array associativo che utilizza il nome dei giocatori come chiave e salva il loro punteggio)
+	#variabili ricevute:
 		#nome_giocatore_corrente
-		#carte_in_mano (array di booleani)
+		#carta_giocata 
 		#carta_evento (numero positivo o negativo)
-		#carte_evento_rimanenti (array di numeri sia negativi che positivi convertito in stringa)
 
 		#servURL (costante)
 		#carattereSeparatore (costante) -> utilizzato per separare i vari campi di un array quando convertito in stringa 
@@ -16,16 +13,25 @@
 		#STRUTTURA ARRAY PASSATO ALLA PAGINA ATTRAVERSO LA FUNZIONE invia_dati:
 
 		#params = array(
-			#n_giocatori -> int n
-			#giocatori-> TUTTO SOTTO FORMA DI STRINGA DA SPLITTARE array(nomi_giocatori, punteggio_giocatori)
-			#nomi_giocatori -> array(char* nomi giocatori)
 			#nome_giocatore_corrente ->char* nome_giocatore_corrente
 			#carta_cliccata -> int nCarta
-			#carte_in_mano -> array(carte)
 			#carta_evento -> int nCarta  
-			#carte_evento_rimanenti -> array di interi sotto forma di stringa
 		#	)	
 
 	
 	#PASSO IL NOME DEL GIOCATORE E LA CARTA GIOCATA AL DB, POI ASPETTO 
-	
+	require "sqlite3.dll";
+
+	#apro il db
+	$db = sqlite_open("partita_accipicchia.db, 066, "impossibile aprire il DB");
+
+	#controllo che l'apertura sia avvenuta con successo
+	if($db)
+	{
+		#in caso positivo aggiorno il la tabella "giocatori" del db
+		$carta_giocata = $_POST['carta_giocata']
+		$nome_giocatore_corrente = $_POST['nome_giocatore_corrente']
+		
+		sqlite_query($db, "	UPDATE giocatori
+			  		SET 	cartaGiocata = ".$carta_giocata."
+			  		WHERE 	nome_giocatore = ".$nome_giocatore_corrente);
